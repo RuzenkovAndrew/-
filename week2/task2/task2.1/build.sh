1ddfcbfd9e848d2c498eb2d474deb8f503d8766a
RuzenkovAndrew <ruzenkovandreu@gmail.com>
2024-10-06 23:41:31 +0300

    Delete week2/task2 directory

--- week2/task2/task2.1/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf bin
-rm -rf obj
-mkdir -p bin
-mkdir -p obj
-g++ -c src/task2_1.cpp -o obj/task2_1.o
-g++ -c src/main.cpp -o obj/main.o
-g++ -o bin/main obj/main.o obj/task2_1.o
